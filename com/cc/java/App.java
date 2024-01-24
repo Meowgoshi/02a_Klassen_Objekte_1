e187bcafed169373c86e089be931cbe67f204bc4
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-24 10:53:50 +0100

    01 / cat

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,9 +2,18 @@ package com.cc.java;
 
 public class App {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        Cat cat = new Cat();    // Instanzierung
+        System.out.println(cat);
+        //cat.sayHello();
+        cat.tellYourAddress();
 
+        System.out.println("----------------------------");
 
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        cat2.tellYourAddress();
     }
 
    
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Cat 
+{
+    public void sayHello()
+    {
+        System.out.println("Meow");
+    }
+
+    public void tellYourAddress()
+    {
+        System.out.println(this);
+    }
+}
