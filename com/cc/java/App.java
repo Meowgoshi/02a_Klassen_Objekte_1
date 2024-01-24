ca456c0c26ff1a724d16a92e568c73024ca11e91
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-24 15:48:37 +0100

    02 / cat finished

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,16 +4,58 @@ public class App {
     
     public static void main(String[] args) 
     {
+        
         Cat cat = new Cat();    // Instanzierung
-        System.out.println(cat);
-        //cat.sayHello();
-        cat.tellYourAddress();
+
+        // Wertzuweisung
+        cat.name = "Legoshi";
+        cat.furColor ="gray";
+        cat.age = 3;
+
+        //Ausgabe
+        // System.out.println(cat);
+        // cat.sayHello();
+        output(cat.tellYourName());
+        output(cat.tellYourFurColor());
+        // System.out.println(cat.tellYourAge()); // int --> string
+        output(String.valueOf(cat.tellYourAge()));
+        // cat.tellYourAddress();
+        // cat.tellYourName();
+        // cat.tellYourFurColor();
+        // cat.tellYourAge();
+
+        
+        System.out.println("----------------------------");
+ 
+        Cat cat2 = new Cat();
+        cat2.name = "Ripley";
+        cat2.furColor = "black";
+        cat2.age = 2;
+ 
+        // System.out.println(cat2);
+        System.out.println(cat2.tellYourName());
+        System.out.println(cat2.tellYourFurColor());
+        System.out.println(cat2.tellYourAge());
+        // cat2.tellYourAddress();
 
         System.out.println("----------------------------");
 
-        Cat cat2 = new Cat();
-        System.out.println(cat2);
-        cat2.tellYourAddress();
+        Cat cat3 = new Cat();
+        cat3.name = "Yuffie";
+        cat3.furColor = "brown";
+        cat3.age = 5;
+
+        // System.out.println(cat3);
+        System.out.println(cat3.tellYourName());
+        System.out.println(cat3.tellYourFurColor());
+        System.out.println(cat3.tellYourAge());
+        // cat2.tellYourAddress();
+        
+    }
+
+     // statische Methode --- in Klasse
+     public static void output(String outputStr){
+        System.out.println(outputStr);
     }
 
    
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,13 +2,38 @@ package com.cc.java;
 
 public class Cat 
 {
+    /*
     public void sayHello()
     {
         System.out.println("Meow");
+    }
+    */
+
+    String name;
+    String furColor;
+    int age;
+
+    public String tellYourName()
+    {
+        // System.out.println(this.name);
+        return this.name;
     }
 
     public void tellYourAddress()
     {
         System.out.println(this);
     }
+
+    public String tellYourFurColor()
+    {
+        // System.out.println(this.furColor);
+        return this.furColor;
+    }
+    
+    public int tellYourAge()
+    {
+        // System.out.println(this.age);
+        return this.age;
+    }
+    
 }
